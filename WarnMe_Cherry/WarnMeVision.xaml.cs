e151891aa0504394c60ea7184bdd74ef18d53cc0
Sebastian Stohr <sebastian.stohr@LIVE.DE>
2018-02-16 12:14:14 +0100

    GUI in XAML hinzugefügt
    GUI in allen Farben erhältlich, solange sie Schwarz ist

--- WarnMe_Cherry/WarnMeVision.xaml.cs
+++ WarnMe_Cherry/WarnMeVision.xaml.cs
@@ -21,10 +21,93 @@ namespace WarnMe_Cherry
     /// </summary>
     public partial class WarnMeVision : Window
     {
+        System.ComponentModel.BackgroundWorker Updater = new System.ComponentModel.BackgroundWorker();
+
+        const string WORKINGDAYS_TABLE_NAME = "Arbeitstage";
+        const string WECKER_TABLE_NAME = "Wecker";
+        const string SETTINGS_TABLE_NAME = "Einstellungen";
+
+        // Shortcuts
+        string HeuteString => DateTime.Now.ToShortDateString();
+        TimeSpan Jetzt => DateTime.Now.TimeOfDay;
+
+        Arbeitstag Heute
+        {
+            get
+            {
+                Arbeitstag heute;
+                if (Datenbank.Exists(WORKINGDAYS_TABLE_NAME, HeuteString))
+                {
+                    heute = Datenbank.Select<Arbeitstag>(WORKINGDAYS_TABLE_NAME, HeuteString);
+                    heute.EndZeit = DateTime.Now.TimeOfDay;
+                }
+                else
+                {
+                    heute = new Arbeitstag()
+                    {
+                        StartZeit = DateTime.Now.TimeOfDay - Extern.UpDuration,
+                        EndZeit = DateTime.Now.TimeOfDay
+                    };
+                    Datenbank.Insert(WORKINGDAYS_TABLE_NAME, HeuteString, heute);
+                }
+                return heute;
+            }
+            set
+            {
+                Datenbank.Update(WORKINGDAYS_TABLE_NAME, HeuteString, value);
+            }
+        }
+
+        Datenbank.Datenbank Datenbank;
+
         public WarnMeVision()
         {
             //System.Threading.Thread.Sleep(1000);
             InitializeComponent();
+        }
+
+        private void AfterFormInitialized(object sender, RoutedEventArgs e)
+        {
+            //Load Datenbank
+            Datenbank = new Datenbank.Datenbank(@".\Configuration.json");
+            InitFormValues();
+
+            //Set up Updater
+            Updater.DoWork += (object o, System.ComponentModel.DoWorkEventArgs arg) => 
+            {
+                System.Threading.Thread.Sleep(1000 - DateTime.Now.Millisecond);
+            };
+            Updater.RunWorkerCompleted += UpdateForm;
+
+            //Set up TimeLine
+            timeLine.MaxValue = new TimeSpan(10, 45, 0).TotalSeconds; // Datenbank.Select<TimeSpan>("Parameter", "MaxWorkingTime").TotalSeconds;
+
+            //Start Updater
+            Updater.RunWorkerAsync();
+        }
+
+        private void UpdateForm(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            // Update Timeline
+            timeLine.Value = (Jetzt - Heute.StartZeit).TotalSeconds;
+
+            // Restart Updater
+            Updater.RunWorkerAsync();
+        }
+
+        private void InitFormValues()
+        {
+
+            // init new day of working
+            Arbeitstag heute = Heute;
+
+            StartTimePicker.DateTime = heute.StartZeit;
+
+            EndTimePicker.DateTime = heute.StartZeit + new TimeSpan(7, 45, 0);
+            MaxEndTimePicker.DateTime = heute.StartZeit + new TimeSpan(10, 45, 0);
+
+            Datenbank.Update("Sonstiges", "SystemUpTime", Extern.UpDuration);
+            Datenbank.Commit();
         }
 
         private void ActivateDragMove(object sender, MouseButtonEventArgs e)
@@ -66,6 +149,37 @@ namespace WarnMe_Cherry
         private void EinstellungenBT_Click(object sender, MouseButtonEventArgs e)
         {
             Tabs.SelectedIndex = 2; // Settings
+        }
+
+        private void TestWebsite(object sender, MouseButtonEventArgs e)
+        {
+            Extensions.Wetter wetter = new Extensions.Wetter();
+        }
+
+        private void ShutdownWarnMe(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            PrepareShutdown();
+        }
+
+        private void PrepareShutdown()
+        {
+            // Refresh "Heute"
+            Arbeitstag heute = Heute;
+            heute.EndZeit = Jetzt;
+            Heute = heute;
+
+            // Commit Values
+            Datenbank.Commit();
+        }
+
+        private void StartTimeUpdated(TimeSpan value)
+        {
+            Arbeitstag heute = Heute;
+            heute.StartZeit = value;
+            Heute = heute;
+
+            EndTimePicker.DateTime = value + new TimeSpan(7, 45, 00);
+            MaxEndTimePicker.DateTime = value + new TimeSpan(10, 45, 00);
         }
     }
 }
