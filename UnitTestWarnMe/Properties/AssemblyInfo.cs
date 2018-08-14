f69353422cba5497d145d629c183141ae4071bc3
Lefguga <Sebastian.Stohr@GMX.de>
2018-08-14 16:31:49 +0200

    Ansatzweise Unittest hinzugefügt

---
+++ UnitTestWarnMe/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestWarnMe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Continental AG")]
+[assembly: AssemblyProduct("UnitTestWarnMe")]
+[assembly: AssemblyCopyright("Copyright © Continental AG 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1be185b-8109-46f5-b1bb-6350b1456d0a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestWarnMe/WorkTableTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarnMe_Cherry.ExterneKlassen;
+
+namespace UnitTestWarnMe
+{
+    [TestClass]
+    public class WorkTableTest
+    {
+        [TestMethod]
+        [Description("")]
+        public void TestMethod1()
+        {
+
+        }
+        
+    }
+}
