b2d5dc29892af8bc20f5154df38c2a21f1407535
Lefguga <Sebastian.Stohr@GMX.de>
2018-12-03 16:42:12 +0100

    GUI und Build
    Kleine Änderungen an GUI
    Buildoption zu Debug/Release

--- WarnMe_Cherry/WarnMeVision.xaml.cs
+++ WarnMe_Cherry/WarnMeVision.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using WarnMe_Cherry.Datenbank;
+using static WarnMe_Cherry.Datenbank.InternalVariables;
 
 namespace WarnMe_Cherry
 {
@@ -11,78 +14,49 @@ namespace WarnMe_Cherry
     /// </summary>
     public partial class WarnMeVision : Window
     {
-        System.ComponentModel.BackgroundWorker Updater = new System.ComponentModel.BackgroundWorker();
+        BackgroundWorker Updater = new BackgroundWorker();
 
-        #region STRUCTS
-        public struct WORKINGDAYS
+        ContextMenu contextMenu = new ContextMenu()
         {
-            public const string NAME = "Arbeitstage";
-            public System.Collections.Generic.Dictionary<string, Arbeitstag> VALUE { get; set; }
-        }
-        public struct WECKER
-        {
-            public const string NAME = "Wecker";
-            public System.Collections.Generic.Dictionary<string, Wecker.Wecker> VALUE { get; set; }
-        }
-        public struct SETTINGS
-        {
-            public const string CONFIG_FILE_NAME = @".\Configuration.json";
-            // TABLE
-            public const string NAME = "Einstellungen";
-            public struct Zeiten
+            Items =
             {
-                public const string NAME = "Zeiten";
-                public struct TIME_A_DAY
-                {
-                    public const string NAME = "Workingtime";
-                    public TimeSpan Value { get; set; }
-                }
-                public struct MAX_TIME_A_DAY
-                {
-                    public const string NAME = "MaxWorkingtime";
-                    public TimeSpan Value { get; set; }
-                }
-                public struct KORREKTUR
-                {
-                    public const string NAME = "Korrektur";
-                    public TimeSpan Value { get; set; }
-                }
+                new MenuItem() { Header="Show" },
+                new MenuItem() { Header="Hide" },
+                new Separator(),
+                new MenuItem() { Header="Close" }
             }
-        }
-        public struct OTHER
+        };
+
+        bool DeviceLocked = false;
+        struct Arbeitstag_Heute
         {
-            public const string NAME = "Sonstiges";
-            public const string SYSTEM_UP_TIME = "SystemUpTime";
-        }
-        #endregion
+            public static DateTime Date;
+            public static Arbeitstag Arbeitstag;
+        };
 
         #region Shortcuts
         string HeuteString => DateTime.Now.ToShortDateString();
+        /// <summary>
+        /// Gibt einen <see cref="DateTime.TimeOfDay"/> Wert mit der aktuellen Uhrzeit zurück.
+        /// </summary>
         TimeSpan Jetzt => DateTime.Now.TimeOfDay;
         #endregion
-
-        Arbeitstag Heute
-        {
-            get
-            {
-                if (Datenbank.Exists(WORKINGDAYS.NAME, HeuteString))
-                {
-                    return Datenbank.Select<Arbeitstag>(WORKINGDAYS.NAME, HeuteString);
-                }
-                throw new ResourceReferenceKeyNotFoundException("Data not found", HeuteString);
-            }
-            set
-            {
-                Datenbank.Update(WORKINGDAYS.NAME, HeuteString, value);
-            }
-        }
-
-        Datenbank.Datenbank Datenbank;
-
+        
         public WarnMeVision()
         {
             //System.Threading.Thread.Sleep(1000);
             InitializeComponent();
+            Übersicht.ZeitTabelle.ValueUpdated += ZeitTabelle_ValueUpdated;
+            Microsoft.Win32.SystemEvents.SessionSwitch += SaveCurrentSessionState;
+        }
+
+        private void SaveCurrentSessionState(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        {
+            switch(e.Reason)
+            {
+                case Microsoft.Win32.SessionSwitchReason.SessionLock: DeviceLocked = true; break;
+                case Microsoft.Win32.SessionSwitchReason.SessionUnlock: DeviceLocked = false; break;
+            }
         }
 
         private void ActivateDragMove(object sender, MouseButtonEventArgs e)
@@ -99,14 +73,21 @@ namespace WarnMe_Cherry
 
         private void FrameworkElement_MouseDown_Focus(object sender, MouseButtonEventArgs e) => ((FrameworkElement)sender).Focus();
 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            PrepareShutdown();
+            // notyIcon?.Dispose();
+            base.OnClosing(e);
+        }
+
         // *********************************************************************************
         // HOME EVENTS
         // *********************************************************************************
 
         private void HomeBT_Click(object sender, MouseButtonEventArgs e)
         {
-            Home.Visibility = Visibility.Visible; // Home
-            Settings.Visibility = Übersicht.Visibility = Visibility.Hidden;
+            Home.StartTimePicker.DateTime = Heute.StartZeit;
+            Tabs.SelectedIndex = 0; //Home
         }
 
         // *********************************************************************************
@@ -115,46 +96,12 @@ namespace WarnMe_Cherry
 
         private void ÜbersichtBT_Click(object sender, MouseButtonEventArgs e)
         {
-            Übersicht.Visibility = Visibility.Visible; // Home
-            Settings.Visibility = Home.Visibility = Visibility.Hidden;
+            Tabs.SelectedIndex = 1; //Übersicht
         }
 
-        private void PrevMonth_Click(object sender, MouseButtonEventArgs e)
+        private void ZeitTabelle_ValueUpdated(DateTime date, Arbeitstag arbeitstag)
         {
-            if (ZeitTabelle.Month == 1)
-            {
-                ZeitTabelle.Month = 12;
-                ZeitTabelle.Year--;
-            }
-            else
-            {
-                ZeitTabelle.Month--;
-            }
-            ZeitTabelle.UpdateTable();
-            MonthTitle.Text = ZeitTabelle.MonthString;
-        }
-
-        private void NextMonth_Click(object sender, MouseButtonEventArgs e)
-        {
-            if (ZeitTabelle.Month == 12)
-            {
-                ZeitTabelle.Month = 1;
-                ZeitTabelle.Year++;
-            }
-            else
-            {
-                ZeitTabelle.Month++;
-            }
-            ZeitTabelle.UpdateTable();
-            MonthTitle.Text = ZeitTabelle.MonthString;
-        }
-
-        private void ResetMonth_Click(object sender, MouseButtonEventArgs e)
-        {
-            ZeitTabelle.Month = DateTime.Now.Month;
-            ZeitTabelle.Year = DateTime.Now.Year;
-            ZeitTabelle.UpdateTable();
-            MonthTitle.Text = ZeitTabelle.MonthString;
+            UpdateWorkingDay(date, arbeitstag);
         }
 
         // *********************************************************************************
@@ -163,8 +110,7 @@ namespace WarnMe_Cherry
 
         private void EinstellungenBT_Click(object sender, MouseButtonEventArgs e)
         {
-            Settings.Visibility = Visibility.Visible; // Home
-            Home.Visibility = Übersicht.Visibility = Visibility.Hidden;
+            Tabs.SelectedIndex = 2; //Einstellungen
         }
 
         private void TestWebsite(object sender, MouseButtonEventArgs e)
@@ -178,46 +124,67 @@ namespace WarnMe_Cherry
 
         private void AfterFormInitialized(object sender, RoutedEventArgs e)
         {
+            // Init Notificytion Icon
+            //notyIcon = new NotificationIcon.NotificationIcon
+            //{
+            //    ContextMenu = contextMenu.ContextMenu,
+            //    ToolTipText = "Hallo"
+            //};
+
             //Load Datenbank
-            Datenbank = new Datenbank.Datenbank(SETTINGS.CONFIG_FILE_NAME);
+            InternalVariables.Datenbank = new Datenbank.Datenbank(SETTINGS.CONFIG_FILE_NAME);
 
             //Set up Variables and Settings
             InitFormValues();
 
             //Set up Updater
-            Updater.DoWork += (object o, System.ComponentModel.DoWorkEventArgs arg) =>
+            Updater.DoWork += (object o, DoWorkEventArgs arg) =>
             {
                 System.Threading.Thread.Sleep(1000 - DateTime.Now.Millisecond);
             };
             Updater.RunWorkerCompleted += UpdateForm;
 
             //Set up TimeLine
-            timeLine.MaxValue = new TimeSpan(10, 45, 0).TotalSeconds; // Datenbank.Select<TimeSpan>("Parameter", "MaxWorkingTime").TotalSeconds;
+            Home.timeLine.MaxValue = new TimeSpan(10, 45, 0).TotalSeconds; // Datenbank.Select<TimeSpan>("Parameter", "MaxWorkingTime").TotalSeconds;
 
             //Start Updater
             Updater.RunWorkerAsync();
         }
 
-        private void UpdateForm(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        private void UpdateForm(object sender, RunWorkerCompletedEventArgs e)
         {
-            // Update Timeline
-            Arbeitstag _heute;
-            try
+            if (!DeviceLocked)
             {
-                _heute = Heute;
+
+                // Update Timeline
+                try
+                {
+                    if (Arbeitstag_Heute.Date != DateTime.Now.Date)
+                    {
+                        UpdateWorkingDay(Arbeitstag_Heute.Date, Arbeitstag_Heute.Arbeitstag);
+                        InitFormValues();
+                    }
+                    else
+                    {
+                        Arbeitstag_Heute.Arbeitstag.EndZeit = Jetzt;
+                    }
+
+                }
+                catch (ResourceReferenceKeyNotFoundException)
+                {
+                    InitFormValues();
+                }
+
+                Home.timeLine.Value = (Jetzt - Arbeitstag_Heute.Arbeitstag.StartZeit).TotalSeconds;
+                Home.timeLine.Update();
+
             }
-            catch (ResourceReferenceKeyNotFoundException)
-            {
-                InitFormValues();
-                _heute = Heute;
-            }
-            timeLine.Value = (Jetzt - _heute.StartZeit).TotalSeconds;
 
             // Restart Updater
             Updater.RunWorkerAsync();
         }
 
-        private void ShutdownWarnMe(object sender, System.ComponentModel.CancelEventArgs e) => PrepareShutdown();
+        // private void ShutdownWarnMe(object sender, System.ComponentModel.CancelEventArgs e) => PrepareShutdown();
 
         // *********************************************************************************
         // PRIVATE FUNCTIONS
@@ -227,10 +194,10 @@ namespace WarnMe_Cherry
         {
 
             // init new day of working
-            if (!Datenbank.Exists(WORKINGDAYS.NAME, HeuteString))
+            if (!InternalVariables.Datenbank.Exists(WORKINGDAYS.NAME, HeuteString))
             {
                 DateTime upTime = Extern.SystemUpTime;
-                Datenbank.Insert(WORKINGDAYS.NAME, HeuteString, new Arbeitstag()
+                InternalVariables.Datenbank.Insert(WORKINGDAYS.NAME, HeuteString, new Arbeitstag()
                 {
                     // check if uptime is today else add the actual time
                     StartZeit = (upTime.Date == DateTime.Now.Date ? upTime.TimeOfDay : Jetzt),
@@ -238,44 +205,51 @@ namespace WarnMe_Cherry
                     Bemerkung = ""
                 });
             }
-            Arbeitstag heute = Heute;
+            Arbeitstag_Heute.Date = DateTime.Now.Date;
+            Arbeitstag_Heute.Arbeitstag = Heute;
 
-            StartTimePicker.DateTime = heute.StartZeit;
+            Home.StartTimePicker.DateTime = Arbeitstag_Heute.Arbeitstag.StartZeit;
 
-            EndTimePicker.DateTime = heute.StartZeit + new TimeSpan(7, 45, 0);
-            MaxEndTimePicker.DateTime = heute.StartZeit + new TimeSpan(10, 45, 0);
+            Home.EndTimePicker.DateTime = Arbeitstag_Heute.Arbeitstag.StartZeit + new TimeSpan(7, 45, 0);
+            Home.MaxEndTimePicker.DateTime = Arbeitstag_Heute.Arbeitstag.StartZeit + new TimeSpan(10, 45, 0);
 
-            Datenbank.Update(OTHER.NAME, OTHER.SYSTEM_UP_TIME, Extern.SystemUpTime);
-            Datenbank.Commit();
+            InternalVariables.Datenbank.Update(OTHER.NAME, OTHER.SYSTEM_UP_TIME, Extern.SystemUpTime);
+            InternalVariables.Datenbank.Commit();
 
             // WORKINGDAY TABLE
-            System.Collections.Generic.Dictionary<DateTime, Arbeitstag> dictionaryAT = Datenbank.Select<Arbeitstag>(WORKINGDAYS.NAME).ToDictionary(item => DateTime.Parse(item.Key), item => item.Value);
+            System.Collections.Generic.Dictionary<DateTime, Arbeitstag> dictionaryAT = InternalVariables.Datenbank.Select<Arbeitstag>(WORKINGDAYS.NAME).ToDictionary(item => DateTime.Parse(item.Key), item => item.Value);
 
-            ZeitTabelle.TableData = new System.Collections.Generic.SortedDictionary<DateTime, Arbeitstag>(dictionaryAT);
+            Übersicht.ZeitTabelle.TableData = new System.Collections.Generic.SortedDictionary<DateTime, Arbeitstag>(dictionaryAT);
 
-            ZeitTabelle.UpdateTable();
+            Übersicht.ZeitTabelle.UpdateTable();
 
+            Einstellungen.Update();
+
+        }
+
+        private void UpdateWorkingDay(DateTime date, Arbeitstag arbeitstag)
+        {
+            InternalVariables.Datenbank.Update(WORKINGDAYS.NAME, date.ToShortDateString(), arbeitstag);
+            InternalVariables.Datenbank.Commit();
         }
 
         private void PrepareShutdown()
         {
             // Refresh "Heute"
-            Arbeitstag heute = Heute;
-            heute.EndZeit = Jetzt;
-            Heute = heute;
+            Arbeitstag_Heute.Arbeitstag.EndZeit = Jetzt;
+            Heute = Arbeitstag_Heute.Arbeitstag;
 
             // Commit Values
-            Datenbank.Commit();
+            InternalVariables.Datenbank.Commit();
+
+            // Close Forms
+            Steuerelemente.WorkTable.window.Close();
         }
 
-        private void StartTimeUpdated(TimeSpan value)
+        private void SettingChanged(string key, object value)
         {
-            Arbeitstag heute = Heute;
-            heute.StartZeit = value;
-            Heute = heute;
-
-            EndTimePicker.DateTime = value + new TimeSpan(7, 45, 00);
-            MaxEndTimePicker.DateTime = value + new TimeSpan(10, 45, 00);
+            InternalVariables.Datenbank.Update(SETTINGS.NAME, key, value);
+            InternalVariables.Datenbank.Commit();
         }
     }
 }
